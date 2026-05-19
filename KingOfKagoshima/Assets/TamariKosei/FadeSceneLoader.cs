21c359f039ce4340bd9ed3ea82d1386af69fea32
tamarikousei <fko2647037@stu.o-hara.ac.jp>
2026-05-19 17:56:50 +0900

    tama

--- KingOfKagoshima/Assets/TamariKosei/FadeSceneLoader.cs
+++ KingOfKagoshima/Assets/TamariKosei/FadeSceneLoader.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class FadeSceneLoader : MonoBehaviour
 {
     public Image fadePanel;              // フェード用のUIパネル（Image）
-    public float fadeDuration = 1.0f;    // フェードの完了にかかる時間
-                                         // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float fadeDuration = 5.0f;    // フェードの完了にかかる時間
+    public bool isPressdEnterKey = false;                                    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
     }
@@ -15,27 +15,29 @@ public class FadeSceneLoader : MonoBehaviour
     {
         fadePanel.enabled = true;               // パネルを有効化
         float elapsedTime = 0.0f;               // 経過時間を初期化
-        Color startColor = fadePanel.color;     // フェードパネルの開始色を取得
-        Color endColor = new Color(startColor.r, startColor.g , startColor.b, 1.0f);
-
+        Color color = fadePanel.color;
         while (elapsedTime < fadeDuration)
         {
-            elapsedTime += Time.deltaTime;                        
-            float t = Mathf.Clamp01(elapsedTime / fadeDuration);  
-            fadePanel.color = Color.Lerp(startColor, endColor, t);
-            yield return null;
+           
+            elapsedTime += Time.deltaTime;                           // 経過時間を増やす                        
+            float t = Mathf.Clamp01(elapsedTime / fadeDuration);     // フェードの進行度を計算  
+            float i = Mathf.Lerp(0, 1, t);   // パネルの色を変更してフェードアウト
+            color.a = i;
+            fadePanel.color = color;
+            yield return null;                                       // 1フレーム待機
         }
-
-        fadePanel.color = endColor;  
+        color.a = 1;
+        fadePanel.color = color;
         SceneManager.LoadScene("StageSelect");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.enterKey.isPressed)
+        if (Keyboard.current.enterKey.isPressed && !isPressdEnterKey)
         {
             StartCoroutine(FadeOutAndLoadScene());
+            isPressdEnterKey = true;
         }
     }
 }
