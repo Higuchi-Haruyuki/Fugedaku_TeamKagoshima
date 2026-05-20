f4dc4f40a5d3c0906f831522754909dc29ebe908
tamarikousei <fko2647037@stu.o-hara.ac.jp>
2026-05-20 12:18:07 +0900

    ta

---
+++ KingOfKagoshima/Assets/TamariKosei/ShowResult.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+
+public class ShowResult : MonoBehaviour
+{
+    // Unityの画面上のテキスト（TextMeshPro）を割り当てる枠
+    public TextMeshProUGUI jumpText;
+
+
+
+    void Start()
+    {
+        // 過去に保存されたデータをPlayerPrefsから直接読み込む
+        // (※一度もジャンプしていない場合は 0 が表示されます)
+        int totalJumps = PlayerPrefs.GetInt("TotalJumpCount", 0);
+
+
+
+        // 画面のテキストを書き換える
+        if (jumpText != null)
+        {
+            jumpText.text = "ジャンプ回数: " + totalJumps + " 回";
+        }
+    }
+}
+
+
+
+
+
+  
